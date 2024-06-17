4e51f34895eff81a783d6d1725ff956dd17466b8
Jacek <69013954+Jacux@users.noreply.github.com>
2024-06-17 23:19:39 +0200

    Update skun-client.lua
    opti

--- client/skun-client.lua
+++ client/skun-client.lua
@@ -35,13 +35,17 @@ function SetDisplay(bool)
 end
 
 Citizen.CreateThread(function()
-    while display do
-        Citizen.Wait(0)
-        DisableControlAction(0, 1, display)
-        DisableControlAction(0, 2, display)
-        DisableControlAction(0, 142, display)
-        DisableControlAction(0, 18, display)
-        DisableControlAction(0, 322, display)
-        DisableControlAction(0, 106, display)
+    while true do
+        local sleep = 1000
+        if display then
+            sleep = 0
+            DisableControlAction(0, 1, display)
+            DisableControlAction(0, 2, display)
+            DisableControlAction(0, 142, display)
+            DisableControlAction(0, 18, display)
+            DisableControlAction(0, 322, display)
+            DisableControlAction(0, 106, display)
+        end
+        Citizen.Wait(sleep)
     end
 end)
